a08e760ec474d83a2153dd3457cc1e8cd488c432
xRave110 <xRave110@protonmail.com>
2026-03-15 10:25:39 +0100

    spl handling

--- programs/my_project/src/lib.rs
+++ programs/my_project/src/lib.rs
@@ -1,4 +1,5 @@
 use anchor_lang::prelude::*;
+use anchor_spl::token_interface::{Mint, TokenAccount, TokenInterface};
 
 declare_id!("6B8tT1EzLMKUZ5fF5H8cTs4We1vLabVeZtgCnB4Ccmnq"); // program ID, best to keep it unchangeable
 // the private key to this is at target/deploy/my_project-keypair.json
@@ -6,37 +7,104 @@ declare_id!("6B8tT1EzLMKUZ5fF5H8cTs4We1vLabVeZtgCnB4Ccmnq"); // program ID, best
 #[program]
 pub mod my_project {
     use super::*;
-
+ 
     pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
         msg!("Greetings from: {:?}", ctx.program_id);
-        msg!("Signer: {}", ctx.accounts.signer.key());
-        let counter = &mut ctx.accounts.counter;
-        counter.value = 0;
-        counter.bump = ctx.bumps.counter;
-
+        msg!("Signer {:?}", ctx.accounts.signer.key());
+ 
+        ctx.accounts.counter.value = 0;
+        ctx.accounts.counter.bump = ctx.bumps.counter;
+ 
+        Ok(())
+    }
+ 
+    pub fn increment(ctx: Context<Increment>, increment_by: u64) -> Result<()> {
+        msg!(
+            "Value incremented by {:?} for {:?}",
+            increment_by,
+            ctx.accounts.signer.key()
+        );
+ 
+        require!(
+            ctx.accounts.source_token_account.amount >= increment_by,
+            MyErrors::AmountTooSmall
+        );
+ 
+        ctx.accounts.counter.value += increment_by;
+ 
+        let cpi_accounts = anchor_spl::token::Transfer {
+            from: ctx.accounts.source_token_account.to_account_info(),
+            to: ctx.accounts.destination_token_account.to_account_info(),
+            authority: ctx.accounts.signer.to_account_info(),
+        };
+ 
+        let cpi_context =
+            CpiContext::new(ctx.accounts.token_program.to_account_info(), cpi_accounts);
+ 
+        anchor_spl::token::transfer(cpi_context, increment_by)?;
+ 
         Ok(())
     }
 }
-
+ 
+#[error_code]
+enum MyErrors {
+    AmountTooSmall,
+}
+ 
 #[derive(Accounts)]
 pub struct Initialize<'info> {
     #[account(mut)]
     pub signer: Signer<'info>,
+ 
     #[account(
-        init, 
-        payer = signer, 
-        space = 8 + Counter::INIT_SPACE, 
-        seeds=[b"counter", 
-        signer.key().to_bytes().as_ref()], 
-        bump)]
+        init,
+        payer = signer,
+        space = 8 + Counter::INIT_SPACE,
+        seeds=[b"counter", signer.key().to_bytes().as_ref()],
+        bump
+    )]
     pub counter: Account<'info, Counter>,
-
+ 
     pub system_program: Program<'info, System>,
 }
-
+ 
+#[derive(Accounts)]
+pub struct Increment<'info> {
+    #[account(mut)]
+    pub signer: Signer<'info>,
+ 
+    #[account(
+        mut,
+        seeds=[b"counter", signer.key().to_bytes().as_ref()],
+        bump = counter.bump
+    )]
+    pub counter: Account<'info, Counter>,
+ 
+    pub vault_authority: SystemAccount<'info>,
+ 
+    #[account(
+        mut,
+        associated_token::mint = token_mint,
+        associated_token::authority = signer,
+    )]
+    pub source_token_account: InterfaceAccount<'info, TokenAccount>,
+ 
+    #[account(
+        mut,
+        associated_token::mint = token_mint,
+        associated_token::authority = vault_authority,
+    )]
+    pub destination_token_account: InterfaceAccount<'info, TokenAccount>,
+ 
+    pub token_mint: InterfaceAccount<'info, Mint>,
+ 
+    pub token_program: Interface<'info, TokenInterface>,
+}
+ 
 #[account]
 #[derive(InitSpace)]
-pub struct Counter{
+pub struct Counter {
     pub value: u64,
-    pub bump: u8
+    pub bump: u8,
 }
--- tests/my_project.ts
+++ tests/my_project.ts
@@ -1,16 +1,138 @@
 import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
 import { MyProject } from "../target/types/my_project";
+import { Keypair, PublicKey, SystemProgram } from "@solana/web3.js";
+import {
+  createMint,
+  createAssociatedTokenAccount,
+  mintTo,
+  TOKEN_PROGRAM_ID,
+} from "@solana/spl-token";
+import { BN } from "bn.js";
+import { assert } from "chai";
 
 describe("my_project", () => {
   // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.AnchorProvider.env());
+  const provider = anchor.AnchorProvider.env();
+  anchor.setProvider(provider);
 
   const program = anchor.workspace.myProject as Program<MyProject>;
+  const connection = provider.connection;
+
+  const vaultAuthority = Keypair.generate();
 
   it("Is initialized!", async () => {
     // Add your test here.
-    const tx = await program.methods.initialize().rpc();
+    const keypair = Keypair.generate();
+
+    const sig = await connection.requestAirdrop(keypair.publicKey, 1e9);
+    const { blockhash, lastValidBlockHeight } =
+      await connection.getLatestBlockhash();
+
+    await connection.confirmTransaction({
+      blockhash,
+      lastValidBlockHeight,
+      signature: sig,
+    });
+
+    const mint = await createMint(
+      connection,
+      provider.wallet.payer,
+      provider.wallet.payer.publicKey,
+      null,
+      6
+    );
+
+    const sourceTokenAccount = await createAssociatedTokenAccount(
+      connection,
+      provider.wallet.payer,
+      mint,
+      keypair.publicKey
+    );
+
+    const destinationTokenAccount = await createAssociatedTokenAccount(
+      connection,
+      provider.wallet.payer,
+      mint,
+      vaultAuthority.publicKey
+    );
+
+    const amountToMint = 1_100_000;
+
+    const mintSignature = await mintTo(
+      connection,
+      provider.wallet.payer,
+      mint,
+      sourceTokenAccount,
+      provider.wallet.payer,
+      amountToMint
+    );
+
+    {
+      const { blockhash, lastValidBlockHeight } =
+        await connection.getLatestBlockhash();
+
+      await connection.confirmTransaction({
+        blockhash,
+        lastValidBlockHeight,
+        signature: mintSignature,
+      });
+    }
+
+    const balance = await connection.getTokenAccountBalance(sourceTokenAccount);
+
+    console.log(balance);
+
+    assert.ok(+balance.value.amount === amountToMint);
+
+    const [counterPdaAddress, counterBump] = PublicKey.findProgramAddressSync(
+      [Buffer.from("counter"), keypair.publicKey.toBuffer()],
+      program.programId
+    );
+    const tx = await program.methods
+      .initialize()
+      .accountsStrict({
+        signer: keypair.publicKey,
+        counter: counterPdaAddress,
+        systemProgram: SystemProgram.programId,
+      })
+      .signers([keypair])
+      .rpc();
+
     console.log("Your transaction signature", tx);
+
+    const accountState = await program.account.counter.fetch(counterPdaAddress);
+    console.log("Signer address", keypair.publicKey.toBase58());
+    console.log(accountState);
+
+    const incrementBy = new BN(2_000_000);
+    await program.methods
+      .increment(incrementBy)
+      .accountsStrict({
+        signer: keypair.publicKey,
+        counter: counterPdaAddress,
+        sourceTokenAccount,
+        destinationTokenAccount,
+        tokenMint: mint,
+        tokenProgram: TOKEN_PROGRAM_ID,
+        vaultAuthority: vaultAuthority.publicKey,
+      })
+      .signers([keypair])
+      .rpc();
+
+    const afterUpdate = await program.account.counter.fetch(counterPdaAddress);
+
+    console.log("After update", afterUpdate);
+
+    assert.ok(afterUpdate.value.eq(incrementBy));
+
+    const sourceBalanceAfter = await connection.getTokenAccountBalance(
+      sourceTokenAccount
+    );
+    const destinationBalanceAfter = await connection.getTokenAccountBalance(
+      destinationTokenAccount
+    );
+
+    console.log(sourceBalanceAfter, destinationBalanceAfter);
   });
 });
